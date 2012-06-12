a2213ff96c88368b5051bf4e3aad5fe57638033b
Sergey Savchuk@SergeySavchukPC.RT-G32 <Sergey Savchuk@SergeySavchukPC.RT-G32>
2012-06-12 23:55:08 +0300

    moving logic to model

--- Poker/MainWindow.xaml.cs
+++ Poker/MainWindow.xaml.cs
@@ -12,10 +12,8 @@ namespace Poker {
     public partial class MainWindow : Window {
 
         private readonly PokerStatRootModel _model = new PokerStatRootModel();
-        private readonly PokerCalculator _calculator = new PokerCalculator();
         private Thread _calcThread;
         private Timer _refreshTimer;
-        private readonly object _sync = new object();
 
         public MainWindow() {
             InitializeComponent();
@@ -35,35 +33,13 @@ namespace Poker {
         }
 
         private void OnTimerTick() {
-            if (_model.Dirty) {
-                lock (_sync) {
-                    _calculator.Reset();
-                    _model.ResetDirty();
-                    foreach (var player in _model.Players) {
-                        var calcPlayer = new PokerCalculatorPlayer();
-                        calcPlayer.CardA = player.CardA.Card;
-                        calcPlayer.CardB = player.CardB.Card;
-                        _calculator.Players.Add(calcPlayer);
-                    }
-                    _calculator.Table.CardA = _model.TableCards.CardA.Card;
-                    _calculator.Table.CardB = _model.TableCards.CardB.Card;
-                    _calculator.Table.CardC = _model.TableCards.CardC.Card;
-                    _calculator.Table.CardD = _model.TableCards.CardD.Card;
-                    _calculator.Table.CardE = _model.TableCards.CardE.Card;
-                }
-            }
-            for (int i = 0; i < _model.Players.Count; i++) {
-                var playerModel = _model.Players[i];
-                var player = _calculator.Players[i];
-                playerModel.Statistics.CopyFrom(player);
-            }
+            _model.Sync();
+
         }
 
         private void CalcThreadEntryPoint() {
             while (true) {
-                lock (_sync) {
-                    _calculator.PlayGame();
-                }
+                _model.PlayGame();
             }
         }
 
--- Poker/Models/PokerStatRootModel.cs
+++ Poker/Models/PokerStatRootModel.cs
@@ -4,9 +4,16 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using Poker.Statistics;
 
 namespace Poker.Models {
-    public class PokerStatRootModel {
+    public class PokerStatRootModel : INotifyPropertyChanged {
+
+        private readonly PokerCalculator _calculator = new PokerCalculator();
+        private readonly PokerCardDeckViewModel _cardDeck = new PokerCardDeckViewModel();
+        private readonly PokerTableCardsViewModel _tableCards = new PokerTableCardsViewModel();
+        private readonly ObservableCollection<PokerPlayerViewModel> _players = new ObservableCollection<PokerPlayerViewModel>();
+        private readonly object _sync = new object();
 
         public PokerStatRootModel() {
             _tableCards.CardA.PropertyChanged += TableCardsOnPropertyChanged;
@@ -36,7 +43,8 @@ namespace Poker.Models {
         private void PlayerCardsOnPropertyChanged(object sender, PropertyChangedEventArgs args) {
             switch (args.PropertyName) {
                 case "Card":
-                    InvalidateState();
+                    _dirty = true;
+                    Sync();
                     break;
             }
         }
@@ -44,7 +52,8 @@ namespace Poker.Models {
         private void TableCardsOnPropertyChanged(object sender, PropertyChangedEventArgs args) {
             switch (args.PropertyName) {
                 case "Card":
-                    InvalidateState();
+                    _dirty = true;
+                    Sync();
                     break;
             }
         }
@@ -74,17 +83,14 @@ namespace Poker.Models {
             if (_tableCards.CardE.IsEmpty) yield return _tableCards.CardE;
         }
 
-        private readonly PokerCardDeckViewModel _cardDeck = new PokerCardDeckViewModel();
         public PokerCardDeckViewModel CardDeck {
             get { return _cardDeck; }
         }
 
-        private readonly PokerTableCardsViewModel _tableCards = new PokerTableCardsViewModel();
         public PokerTableCardsViewModel TableCards {
             get { return _tableCards; }
         }
 
-        private readonly ObservableCollection<PokerPlayerViewModel> _players = new ObservableCollection<PokerPlayerViewModel>();
         public ObservableCollection<PokerPlayerViewModel> Players {
             get { return _players; }
         }
@@ -94,13 +100,54 @@ namespace Poker.Models {
             get { return _dirty; }
         }
 
-        public void ResetDirty() {
-            _dirty = false;
+        private int _gamesPlayed;
+        public int GamesPlayed {
+            get { return _gamesPlayed; }
+            set {
+                if (_gamesPlayed != value) {
+                    _gamesPlayed = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("GamesPlayed"));
+                }
+            }
         }
 
-        public void InvalidateState() {
-            _dirty = true;
+        public void Sync() {
+            if (_dirty) {
+                lock (_sync) {
+                    _calculator.Reset();
+                    _dirty = false;
+                    foreach (var player in _players) {
+                        var calcPlayer = new PokerCalculatorPlayer { CardA = player.CardA.Card, CardB = player.CardB.Card };
+                        _calculator.Players.Add(calcPlayer);
+                    }
+                    _calculator.Table.CardA = _tableCards.CardA.Card;
+                    _calculator.Table.CardB = _tableCards.CardB.Card;
+                    _calculator.Table.CardC = _tableCards.CardC.Card;
+                    _calculator.Table.CardD = _tableCards.CardD.Card;
+                    _calculator.Table.CardE = _tableCards.CardE.Card;
+                }
+            }
+            for (int i = 0; i < _players.Count; i++) {
+                var playerModel = _players[i];
+                var player = _calculator.Players[i];
+                playerModel.Statistics.CopyFrom(player);
+            }
         }
+
+        public void PlayGame() {
+            lock (_sync) {
+                _calculator.PlayGame();
+            }
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) {
+            var handler = PropertyChanged;
+            if (handler != null) {
+                handler(this, args);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
     }
 }
