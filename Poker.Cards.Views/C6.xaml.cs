c5cfc3a0ce60a9823c3de721b78d78939292fb83
Sergey Savchuk@SergeySavchukHP.Dlink <Sergey Savchuk@SergeySavchukHP.Dlink>
2012-06-12 12:38:47 +0300

    cards

---
+++ Poker.Cards.Views/C6.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Poker.Cards.Views;
+
+namespace Poker.Cards.Views {
+    /// <summary>
+    /// Interaction logic for C2.xaml
+    /// </summary>
+    public partial class C6 : BaseCardView {
+        public C6() {
+            InitializeComponent();
+        }
+    }
+}
