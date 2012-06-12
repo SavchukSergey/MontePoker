b19753cce9f33ac3eedf1c34019c379188309611
Sergey Savchuk@SergeySavchukPC.RT-G32 <Sergey Savchuk@SergeySavchukPC.RT-G32>
2012-06-12 08:21:34 +0300

    cards

---
+++ Poker/Views/Cards/CQ.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Poker.Views.Cards {
+    /// <summary>
+    /// Interaction logic for C2.xaml
+    /// </summary>
+    public partial class CQ : BaseCardView {
+        public CQ() {
+            InitializeComponent();
+        }
+    }
+}
