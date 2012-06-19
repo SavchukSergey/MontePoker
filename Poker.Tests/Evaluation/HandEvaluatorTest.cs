3f1783b9a6b27730418af889939fb35f6116fff3
Sergey Savchuk@SergeySavchukPC.RT-G32 <Sergey Savchuk@SergeySavchukPC.RT-G32>
2012-06-19 23:28:42 +0300

    tests

--- Poker.Tests/Evaluation/HandEvaluatorTest.cs
+++ Poker.Tests/Evaluation/HandEvaluatorTest.cs
@@ -8,47 +8,11 @@ namespace Poker.Tests.Evaluation {
     public class HandEvaluatorTest {
 
         [Test]
-        public void EvaluateHighCard() {
-            var cards = PokerCard.ParseList("AH 9C 10C 2D 4S 7S JS");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.HighCard, eval.HandType);
-        }
-
-        [Test]
-        public void EvaluateOnePairCard() {
-            var cards = PokerCard.ParseList("AH AC 10C 2D 4S 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.OnePair, eval.HandType);
-        }
-
-
-        [Test]
-        public void EvaluateTwoPairsCard() {
-            var cards = PokerCard.ParseList("AH AC 10C 10D 4S 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.TwoPairs, eval.HandType);
-        }
-
-        [Test]
-        public void EvaluateThreeOfKindCard() {
-            var cards = PokerCard.ParseList("AH 10S 10C 10D 4S 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.ThreeOfKind, eval.HandType);
-        }
-
-        [Test]
-        public void EvaluateStraightCard() {
-            var cards = PokerCard.ParseList("JH 10S 10C 10D 8S 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.Straight, eval.HandType);
-        }
-
-        [Test]
+        [TestCase(HandType.HighCard, "AH 9C 10C 2D 4S 7S JS", Description = "High card")]
+        [TestCase(HandType.OnePair, "AH AC 10C 2D 4S 9C 7C", Description = "One pair")]
+        [TestCase(HandType.TwoPairs, "AH AC 10C 10D 4S 9C 7C", Description = "Two pairs")]
+        [TestCase(HandType.ThreeOfKind, "AH 10S 10C 10D 4S 9C 7C", Description = "Three of Kind")]
+        [TestCase(HandType.Straight, "JH 10S 10C 10D 8S 9C 7C", Description = "Straight")]
         [TestCase(HandType.Flush, "AC 10S 10C 10D 4C 9C 7C", Description = "Flush")]
         [TestCase(HandType.FullHouse, "AH AC 10C 10D 10S 9C 7C", Description = "Full House")]
         [TestCase(HandType.FourOfKind, "AH 10S 10C 10D 4S 9C 10H", Description = "Four of Kind")]
@@ -66,6 +30,11 @@ namespace Poker.Tests.Evaluation {
         [TestCase("1 AH AC JC 2D 4S 9C 7C", "0 AH AC 10C 2D 4S 9C 7C", Description = "One Pair - Kicker")]
         [TestCase("1 AH AC 10C 10D 4S JC 7C", "0 AH AC 10C 10D 4S 9C 7C", Description = "Two Pairs - Kicker")]
         [TestCase("1 AH 10S 10C 10D 4S 9C 7C", "0 KH 10S 10C 10D 4S 9C 7C", Description = "Three of Kind - Kicker")]
+        [TestCase("1 JH 10S 10C 10D 8S 9C QC", "0 JH 10S 10C 10D 8S 9C 7C", Description = "Straight - Kicker")]
+        [TestCase("1 AC 10S 10C 10D 4C 9C 7C", "0 KC 10S 10C 10D 4C 9C 7C", Description = "Flush - Kicker")]
+        [TestCase("1 AH AC AD 10D 10S 9C 7C", "0 AH AC 10C 10D 10S 9C 7C", Description = "Full House - Kicker")]
+        [TestCase("1 AH 10S 10C 10D 4S 9C 10H", "0 AH 9S 9C 9D 4S 9H 10H", Description = "Four of Kind - Kicker")]
+        [TestCase("1 7S 3S 4S AC 5S JD 6S", "0 2S 3S 4S AC 5S JD 6S", Description = "Straight Flush - Kicker")]
         [TestCase("1 AD KD 2C JD 10D 4S QD", "1 AH KH 2C JH 10H 4S QH", Description = "Royal Flush - Kicker")]
         public void OneKickerTest(params string[] cardsList) {
             var bestEval = new HandEvaluation { Scores = -1 };
