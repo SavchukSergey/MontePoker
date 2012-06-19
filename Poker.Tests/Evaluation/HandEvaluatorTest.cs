8e007219289a4f2ac6a22fc2df2a7b84f0abc518
Sergey Savchuk@SergeySavchukPC.RT-G32 <Sergey Savchuk@SergeySavchukPC.RT-G32>
2012-06-19 23:21:59 +0300

    tests

--- Poker.Tests/Evaluation/HandEvaluatorTest.cs
+++ Poker.Tests/Evaluation/HandEvaluatorTest.cs
@@ -49,22 +49,8 @@ namespace Poker.Tests.Evaluation {
         }
 
         [Test]
-        public void EvaluateFlushCard() {
-            var cards = PokerCard.ParseList("AC 10S 10C 10D 4C 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.Flush, eval.HandType);
-        }
-
-        [Test]
-        public void EvaluateFullHouseCard() {
-            var cards = PokerCard.ParseList("AH AC 10C 10D 10S 9C 7C");
-            HandEvaluation eval;
-            HandEvaluator.EvaluateHand(cards, out eval);
-            Assert.AreEqual(HandType.FullHouse, eval.HandType);
-        }
-
-        [Test]
+        [TestCase(HandType.Flush, "AC 10S 10C 10D 4C 9C 7C", Description = "Flush")]
+        [TestCase(HandType.FullHouse, "AH AC 10C 10D 10S 9C 7C", Description = "Full House")]
         [TestCase(HandType.FourOfKind, "AH 10S 10C 10D 4S 9C 10H", Description = "Four of Kind")]
         [TestCase(HandType.StraightFlush, "2S 3S 4S AC 5S JD 6S", Description = "Straight Flush")]
         [TestCase(HandType.RoyalFlush, "AD KD 2C JD 10D 4S QD", Description = "Royal Flush")]
@@ -80,6 +66,7 @@ namespace Poker.Tests.Evaluation {
         [TestCase("1 AH AC JC 2D 4S 9C 7C", "0 AH AC 10C 2D 4S 9C 7C", Description = "One Pair - Kicker")]
         [TestCase("1 AH AC 10C 10D 4S JC 7C", "0 AH AC 10C 10D 4S 9C 7C", Description = "Two Pairs - Kicker")]
         [TestCase("1 AH 10S 10C 10D 4S 9C 7C", "0 KH 10S 10C 10D 4S 9C 7C", Description = "Three of Kind - Kicker")]
+        [TestCase("1 AD KD 2C JD 10D 4S QD", "1 AH KH 2C JH 10H 4S QH", Description = "Royal Flush - Kicker")]
         public void OneKickerTest(params string[] cardsList) {
             var bestEval = new HandEvaluation { Scores = -1 };
             foreach (var playerInfo in cardsList) {
