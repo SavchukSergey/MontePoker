447a078a51c47ff36046fe3586f8702b84670420
Sergey Savchuk@SergeySavchukPC <Sergey Savchuk@SergeySavchukPC>
2012-11-28 22:01:32 +0200

    sync

---
+++ Poker/Cards/FastCardDeck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poker.Cards {
+    public class FastCardDeck {
+
+        private ulong _busyCards;
+        private byte _cardsDealt;
+
+        private static readonly byte[] _bitsCountMap;
+
+        static FastCardDeck() {
+            _bitsCountMap = new byte[256];
+            for (var i = 0; i < 256; i++) {
+                var count = 0;
+                for (var j = 0; j < 8; j++) {
+                    if ((i & (1 << j)) != 0) count++;
+                }
+                _bitsCountMap[i] = (byte)count;
+            }
+        }
+
+        public bool DealOne(out PokerCard card) {
+            var cardsLeft = 52 - _cardsDealt;
+            var cardIndex = Rnd.Next() % cardsLeft;
+            _cardsDealt++;
+            card = new PokerCard();
+            return true;
+        }
+
+    }
+}
---
+++ Poker/Cards/Rnd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poker.Cards {
+    public static class Rnd {
+
+        private static readonly Random _rnd = new Random(DateTime.Now.Millisecond);
+
+        public static int Next() {
+            return _rnd.Next();
+        }
+    }
+}
